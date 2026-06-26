af6e472b4272c32e2900ef9ada8bb3e368644085
Xoulomon <soulomonjohn221@gmail.com>
2026-06-26 10:51:31 +0100

    feat: export ScVal helper functions from index.ts
    - Export i128ToScVal, u128ToScVal, u64ToScVal, u32ToScVal, strToScVal, addrToScVal
    - Allows consumers to build custom contract interactions
    - All types (MarcConfig, Address, etc) were already exported via export * from types
    - Closes #42

--- sdk/src/index.ts
+++ sdk/src/index.ts
@@ -4,4 +4,14 @@ export { CommerceClient } from "./commerce.js";
 export { marcPaywall, type MarcPaywallOptions } from "./marcPaywall.js";
 export { marcFetch, type MarcFetchOptions } from "./marcFetch.js";
 
+// ScVal encoding helpers for custom contract interactions
+export {
+  i128ToScVal,
+  u128ToScVal,
+  u64ToScVal,
+  u32ToScVal,
+  strToScVal,
+  addrToScVal,
+} from "./commerce.js";
+
 export const MARC_STELLAR_SDK_VERSION = "0.1.0";
